667e4a8baef08c2333a16267efb1ece5f52a7589
jaekwon <kwonj1234@gmail.com>
2023-10-19 22:02:05 -0500

    fix typo

--- src/app/AboutMe/page.tsx
+++ src/app/AboutMe/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   const descrip: String = `Environmental engineer, healthcare provider, air quality inspector turned software developer. 
   I began learning Python on my own prior to enrolling in Byte Academy in 2020 where I learned
   Flask, SQL, JavaScript and React. Since then, I've had the opportunity to learn new 
-  technologoies like Vue, Firebase, Google Cloud Platform (GCP), AWS, Django, Postgres. I've also had
+  technologoies like Vue, Firebase, Google Cloud Platform (GCP), Amazon Web Services (AWS), Django, Postgres. I've also had
   the opportunity to work with great coworkers and mentors alike at Cynopsis Solutions, PlateRate,
   and at my current role at Syft. When I am not working I enjoy playing chess, 
   video games and triathlon sports.` 
