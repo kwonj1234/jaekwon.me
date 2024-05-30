c9d128eabd031d15049c6a225fc32a825bb767ab
kwonj1234 <kwonj1234@gmail.com>
2024-05-30 13:00:24 -0500

    adjust spacing on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <footer className="bottom-0">
+    <footer className="">
       <div className='row justify-center'>
         <h1>Contact Me</h1>
       </div>
