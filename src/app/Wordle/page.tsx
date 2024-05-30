02101ab0bfc143ec0f31d0837d4776a6dd11df2b
kwonj1234 <kwonj1234@gmail.com>
2024-05-30 14:47:44 -0500

    commit

--- src/app/Wordle/page.tsx
+++ src/app/Wordle/page.tsx
@@ -1,16 +1,35 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import axios from "axios";
 
-export default function Projects() {
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function Wordle() {
+  const [isLoading , setIsLoading] = useState<boolean>(true);
+  const [currentWord, setCurrentWord] = useState<string>("");
+  const [targetWord, setTargetWord] = useState<string>("");
+
+  const randomWordAPI: string = "https://random-word-api.herokuapp.com/word?length=5";
+  const checkWordAPI: string = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
   useEffect(() => {
-
+    const getRandomWord = async () => {
+      const word = await axios.get(randomWordAPI)
+      console.log(word)
+    }
+  
+    getRandomWord()
   }, [])
 
   return (
     <div className='Wordle'>
-
+      {
+        isLoading ?
+          <CircularProgress className="m-auto"/>
+        :
+          <div></div>
+      }
     </div>
   )
 }
