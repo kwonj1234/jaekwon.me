feadce25cef476ad14a4ae79daa09feb78b9a7a2
kwonj1234 <kwonj1234@gmail.com>
2024-05-30 22:45:04 -0500

    working on wordle

--- src/app/Wordle/page.tsx
+++ src/app/Wordle/page.tsx
@@ -6,10 +6,12 @@ import axios from "axios";
 import Keyboard from "@/components/Keyboard";
 import CircularProgress from '@mui/material/CircularProgress';
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 export default function Wordle() {
   const [isLoading , setIsLoading] = useState<boolean>(true);
-  const [currentWord, setCurrentWord] = useState<string>("");
   const [targetWord, setTargetWord] = useState<string>("");
+  const [currentWords, setCurrentWords] = useState<Array<string>>([""]);
 
   const randomWordAPI: string = "https://random-word-api.herokuapp.com/word?length=5";
   const checkWordAPI: string = "https://api.dictionaryapi.dev/api/v2/entries/en/";
@@ -25,6 +27,37 @@ export default function Wordle() {
     getRandomWord()
   }, [])
 
+  const getCorrectLetter = (wordIdx: number, letterIdx: number) => {
+    if (wordIdx <= currentWords.length - 1) {
+      if (letterIdx <= currentWords[wordIdx].length - 1) {
+        return currentWords[wordIdx][letterIdx]
+      }
+    }
+    return null
+  }
+
+  const updateCurrentWords = (letter: string) => {
+    let i = currentWords.length - 1
+    if (letter === "Backspace") {
+      setCurrentWords(currentWords.map((word, idx) => {
+        if (idx === i) {
+          return word.substring(0, word.length - 1)
+        }
+        return word
+      }))
+
+    } else if (alphabet.includes(letter) && currentWords[i].length < 5) {
+      console.log(letter)
+      console.log(currentWords)
+      setCurrentWords(currentWords.map((word, idx) => {
+        if (idx === i) {
+          return word + letter
+        } 
+        return word
+      }))
+    }
+  }
+
   return (
     <div className='Wordle'>
       <div className="words-container">
@@ -34,11 +67,11 @@ export default function Wordle() {
         :
           <div>
           {
-            [...Array(6)].map((el, i) => 
-              <div className="word row" key={`word-${i}`}>
+            [...Array(6)].map((el, wordIndex) => 
+              <div className="word row" key={`word-${wordIndex}`}>
                 {
-                  [...Array(5)].map((el, i) => 
-                    <CheckLetter key={`letter-${i}`}></CheckLetter>
+                  [...Array(5)].map((el, letterIndex) => 
+                    <CheckLetter key={`letter-${letterIndex}`} letter={getCorrectLetter(wordIndex, letterIndex)}></CheckLetter>
                   )
                 }
               </div>
@@ -48,16 +81,20 @@ export default function Wordle() {
       }
       </div>
       <Keyboard
-        onClick={() => {}}
+        onClick={updateCurrentWords}
       ></Keyboard>
     </div>
   )
 }
 
-function CheckLetter() {
+interface CheckLetterProps {
+  letter: string | null
+}
+
+function CheckLetter(props: CheckLetterProps) {
   return (
     <div className="letter-border">
-
+      {props.letter}
     </div>
   )
 }
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,34 +1,44 @@
 import BackspaceIcon from '@mui/icons-material/Backspace';
+import { useEffect } from 'react';
 
 interface KeyboardProps {
   onClick: Function,
 }
 
 const qwertyKeyboard = [
-  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-  ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "bk"]
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  ["Enter", "z", "x", "c", "v", "b", "n", "m", "Backspace"]
 ]
 export default function Keyboard(props: KeyboardProps) {
 
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      props.onClick(e.key)
+    })
+  }, [])
+
+
   const keyContainer = (letter: string) => {
-    if (letter === "bk") {
+    if (letter === "Backspace") {
       return (
         <div 
           className={`large-key key-container keyboard-key-${letter}`} 
           key={`keyboard-key-${letter}`}
+          onClick={() => {props.onClick(letter)}}
         >
           <BackspaceIcon className='m-auto'/>
         </div>
       )
 
-    } else if (letter === "ENTER") {
+    } else if (letter === "Enter") {
       return (
         <div 
           className={`large-key key-container keyboard-key-${letter}`} 
           key={`keyboard-key-${letter}`}
+          onClick={() => {props.onClick(letter)}}
         >
-          <h1 className='m-auto'>{letter}</h1>
+          <h1 className='m-auto'>{letter.toUpperCase()}</h1>
         </div>
       )
 
@@ -37,8 +47,9 @@ export default function Keyboard(props: KeyboardProps) {
         <div 
           className={`small-key key-container keyboard-key-${letter}`} 
           key={`keyboard-key-${letter}`}
+          onClick={() => {props.onClick(letter)}}
         >
-          <h1 className='m-auto' >{letter}</h1>
+          <h1 className='m-auto' >{letter.toUpperCase()}</h1>
         </div>
       )
     }
